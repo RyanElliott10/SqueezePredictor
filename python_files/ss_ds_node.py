6ec202344f7e3620a512091e3c6cdf7d0de928c4
Ryan Elliott <ryan.elliott31@gmail.com>
2018-01-05 16:35:24 -0600

    Restructured files.

---
+++ python_files/ss_ds_node.py
@@ -0,0 +1,60 @@
+class Node:
+
+    def __init__(self, ticker, name, prev_close, open_p, curr):
+        self.next = None
+        self.prev = None
+        self.ticker = ticker
+        self.name = name
+        self.prev_close = prev_close
+        self.open = open_p
+        self.curr_price = curr
+        self.perc_change = 0
+        self.avg_volume = 0
+        self.vol_uptrend = True
+        self.price_uptrend = True
+        self.high_shorts = False
+        self.high_beta = False
+        self.days_twenty_perc_above_avg_volume = 0
+        self.shorts_percent_float = 0
+        self.shorts_pain = 0
+        self.days_to_cover = 0
+        self.yearly_low = False
+
+    def get_ticker(self):
+        return self.ticker
+
+    def get_next(self):
+        return self.next
+
+    def get_prev(self):
+        return self.prev
+
+    def get_name(self):
+        return self.name
+
+    def get_prev_close(self):
+        return self.prev_close
+
+    def get_open(self):
+        return self.open
+
+    def set_ticker(self, ticker):
+        self.ticker = ticker
+
+    def set_name(self, name):
+        self.name = name
+
+    def set_next(self, new_next):
+        self.next = new_next
+
+    def set_prev(self, new_prev):
+        self.prev = new_prev
+
+    def set_prev_close(self, close):
+        self.prev_close = close
+
+    def set_open(self, open_p):
+        self.open = open_p
+
+    def set_close(self, close):
+        self.close = close
---
+++ python_files/test.py
@@ -0,0 +1,111 @@
+# Import all the important libraries to parse the HTML page
+
+import csv
+import requests
+
+import matplotlib.pyplot as plt
+from matplotlib import style
+
+#from matplotlib.finance import candlestick_ohlc
+import matplotlib.dates as mdates
+import pandas as pd
+import pandas_datareader.data as web
+from yahoo_finance import Share
+
+#from datetime import date
+import datetime
+import time
+
+
+import bs4
+
+from urllib.request import urlopen as uReq
+from bs4 import BeautifulSoup as soup
+# ^ all of the imports for BeautifulSoup
+
+
+
+url = 'https://finance.yahoo.com/quote/INSY?p=INSY'
+
+sleep_cont = 0
+cont = True
+
+while cont:
+    try:
+        client_page = uReq(url)
+        webpage = client_page.read()
+        cont = False
+    except:
+        sleep_cont += 1
+        time.sleep(5)
+        if sleep_cont > 5:
+            self.write_list.append('Something seems to be wrong with your connection\n')
+
+client_page.close()
+page = soup(webpage, 'html.parser')
+
+#curr = page.findAll('tr')[5].text[13:]
+curr = page.findAll('span')[9].text
+
+print(curr)
+
+low = ''
+count = 0
+for char in curr:
+    count += 1
+    if char == ' ':
+        break
+    else:
+        low += char
+
+high = float(curr[count + 1:])
+low = float(low)
+yearly_low = False
+
+if ((high - low) / 13.38) <= 0.15:
+    yearly_low = True
+
+print(yearly_low)
+
+
+"""start = datetime.datetime(2017, 12, 10)
+cont = True
+while cont:
+    try:
+        df = web.DataReader('CCHI', 'yahoo', start)
+        cont = False
+    except:
+        pass
+print(df)"""
+
+
+
+
+
+
+
+today = datetime.date.today()
+tomorrow = today + datetime.timedelta(days=1)
+
+# [month, day]
+closed_dates = [(1, 1), (1, 15), (2, 19), (3, 30), (5, 28), (7, 4), (9, 3), (11, 22), (12, 25)]
+
+time_delta = 1
+
+# tomorrow.weekday() returns int from 0-6, where 0 is Monday and 6 is Sunday
+if tomorrow.weekday() == 5:
+    time_delta += 2
+
+tomorrow = today + datetime.timedelta(days=time_delta)
+month = tomorrow.month
+day = tomorrow.day
+
+for date in closed_dates:
+    if month == date[0] and day == date[1]:
+        time_delta += 1
+
+tomorrow = today + datetime.timedelta(days=time_delta)
+
+year = str(tomorrow.year)[2:]
+month = str(tomorrow.month)
+day = str(tomorrow.day)
