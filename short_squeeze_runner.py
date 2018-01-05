d0d3f814de043d388e32f7e8784691a5325c2b5a
Ryan Elliott <ryan.elliott31@gmail.com>
2018-01-04 21:04:40 -0600

    Added functions to manually enter tickers, or enter them from CSVs.

--- short_squeeze_runner.py
+++ short_squeeze_runner.py
@@ -22,26 +22,38 @@ from yahoo_finance import Share
 
 
 
-# YOU REALLY SHOULD WRITE A METHOD TO ALLOW YOU TO MANUALLY ENTER TICKERS INTO THE WATCHLIST
-
-
 hash_table = short_squeeze_ds.Hash()
 
 csv_lst = ['./CSV_files/companylist.csv', './CSV_files/companylist (1).csv', './CSV_files/companylist (2).csv']
-#csv_lst = ['./CSV_files/companylist (1).csv']
+#csv_lst2 = ['./CSV_files/companylist (1).csv']
+manual_tickers = []
 
-
-for f in csv_lst:
-    with open(f, 'rt') as filename:
-        reader = csv.reader(filename, 'excel')
-        for i, row in enumerate(reader):
-            #if i == 500:
-                #break
-            if row[0] != 'Symbol' and '^' not in row[0] and '.' not in row[0] and row[2] != 'n/a' and float(row[2]) <= 10 and len(row[0]) < 5 and row[0] != 'TVIX' and row[0] != 'VIIZ' and 'M' in row[3]:
-                    hash_table.insert(row[0])
+insert_from_csv(csv_lst)
 
 #hash_table.insert('AMD')
 
 print('Total tickers to search:', hash_table.num_items, '\n')
 hash_table.init_run()
 hash_table.check_watchlist()
+
+
+
+def insert_from_csv(csv_lst, stop_num=999999):
+    """ Takes in a list of CSV files that are then inserted. """
+
+    for f in csv_lst:
+        with open(f, 'rt') as filename:
+            reader = csv.reader(filename, 'excel')
+            for i, row in enumerate(reader):
+                if i == stop_num:
+                    break
+                if row[0] != 'Symbol' and '^' not in row[0] and '.' not in row[0] and row[2] != 'n/a' and float(row[2]) <= 10 and len(row[0]) < 5 and row[0] != 'TVIX' and row[0] != 'VIIZ' and 'M' in row[3]:
+                        hash_table.insert(row[0])
+
+
+def manual_insert(lst):
+    """ Allows the user to manually enter tickers, mainly for testing. """
+
+    for i, row in enumerate(reader):
+        if row[0] != 'Symbol' and '^' not in row[0] and '.' not in row[0] and row[2] != 'n/a' and float(row[2]) <= 10 and len(row[0]) < 5 and row[0] != 'TVIX' and row[0] != 'VIIZ' and 'M' in row[3]:
+                hash_table.insert(row[0])
